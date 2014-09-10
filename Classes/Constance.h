b32f38925545803b1c911462bc4fd486f121b10d
XiongJian <420048248@qq.com>
2014-09-10 22:35:55 +0800

    create

---
+++ Classes/Constance.h
@@ -0,0 +1,27 @@
+//
+//  Constance.h
+//  BirdTest
+//
+//  Created by Static on 14-8-31.
+//
+//
+
+#ifndef HelloTest_Constance_h
+#define HelloTest_Constance_h
+
+#include "SimpleAudioEngine.h"
+#include "extensions/cocos-ext.h"
+#include "AtlasLoader.h"
+
+USING_NS_CC;
+USING_NS_CC_EXT;
+using namespace CocosDenshion;
+using namespace std;
+
+//  Common Property
+#pragma mark -- Common Property ---
+#define VISIBLE_SIZE Director::getInstance()->getVisibleSize()
+#define VISIBLE_ORIGIN Director::getInstance()->getVisibleOrigin()
+#define WIN_SIZE Director::getInstance()->getWinSize()
+
+#endif
